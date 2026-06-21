30f50d227cda1df757b71ecae0279e69000a9137
sanskargangwar <145826905+sanskargangwar@users.noreply.github.com>
2026-06-21 16:27:17 +0530

    Update server URL to production environment

--- admin/src/context/AuthContext.jsx
+++ admin/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext } from 'react'
 
 export const authDataContext = createContext()
 function AuthContext({children}) {
-    let serverUrl = "http://localhost:8000"
+    let serverUrl = "https://ecommercebackend-gkdn.onrender.com"
 
     let value = {
       serverUrl
